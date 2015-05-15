edd117310d26afb931f19b0ed4141b6cd29955f9
Jonathan Hoyt <hoyt@github.com>
2015-05-15 12:01:14 -0700

    First passing test for docquery class

--- test/docquery_test.js
+++ test/docquery_test.js
@@ -1,10 +1,11 @@
 var assert = require("assert")
+var docquery = require("../src/docquery")
 
-describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', () => {
-      assert.equal(-1, [1,2,3].indexOf(5))
-      assert.equal(-1, [1,2,3].indexOf(0))
+describe("docquery", () => {
+  describe('#directoryPath', () => {
+    it("returns path passed into the contructor", () => {
+      var docqueryInstance = new docquery("/foo/bar")
+      assert.deepEqual(["/foo/bar"], docqueryInstance.directoryPaths)
     })
   })
 })
