99022ac5eb2a88ff0d0bf7d6b8b0244a47c2c797
Jonathan Hoyt <hoyt@github.com>
2015-05-21 23:56:06 -0700

    Add default options from ~/.dq feature

--- src/cli.js
+++ src/cli.js
@@ -25,17 +25,53 @@ if(argv.h) {
   process.exit(0)
 }
 
+let fs = require("fs")
+let tilde = require("tilde-expansion")
 let DocQuery = require("./DocQuery")
-let execPath = argv.p ? argv.p : process.cwd()
-let options = {
-  recursive: argv.r,
-  includeBody: argv.b,
-  persistent: false
-}
-let dq = new DocQuery(execPath, options)
 let delay = function(fn) {
   setTimeout(fn, 500)
 }
+
+// Read default options from ~/.dq
+let defaultOptions = {}
+tilde("~/.dq", function(path) {
+  if(fs.existsSync(path)) {
+    defaultOptions = JSON.parse(fs.readFileSync(path, {encoding: "utf8"}))
+  }
+})
+
+let options = {
+  recursive: false,
+  includeBody: false
+}
+
+// Should dq search sub directories?
+if(argv.r) {
+  options.recursive = true
+}else if(defaultOptions.hasOwnProperty("recursive")) {
+  options.recursive = defaultOptions.recursive
+}else{
+  options.recursive = false
+}
+
+// Should search result include body?
+if(argv.b) {
+  options.includeBody = true
+}else if(defaultOptions.hasOwnProperty("includeBody")) {
+  options.includeBody = defaultOptions.includeBody
+}else{
+  options.includeBody = false
+}
+
+// Use path from args first, configured default second, or current path last.
+let execPath = argv.p
+if(!execPath) execPath = defaultOptions.searchPath
+if(!execPath) execPath = process.cwd
+
+// Create the DocQuery instance.
+let dq = new DocQuery(execPath, options)
+
+// Pause before running the query to ensure data has been indexed.
 delay(function() {
   let results = dq.search(argv._.join(" "))
   let output = JSON.stringify(results, null, 2)
